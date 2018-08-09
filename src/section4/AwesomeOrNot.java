03942cf1255cd3cf91a47d4fddc43102622dacb6
MairaCCB <league@WTS21.attlocal.net>
2018-08-09 12:01:04 -0700

    day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,63 @@ package section4;
 
 import java.util.Random;
 
-public class AwesomeOrNot {
+import javax.swing.JOptionPane;
 
+public class AwesomeOrNot {
+	
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+int random = new Random().nextInt(4);
 
-	// 3. Print out this variable
+// 3. Print out this variable
+System.out.println(random);
+	
+// TODO Auto-generated method stub
+String answer = JOptionPane.showInputDialog("Gimmie a word");
 
-	// 4. Get the user to enter something that they think is awesome
+// 4. Get the user to enter something that they think is awesome
 
-	// 5. If the random number is 0
+	
+// 5. If the random number is 0
 
-	// -- tell the user whatever they entered is awesome!
+	if (random == 0) {
+		JOptionPane.showMessageDialog(null, answer + " Is AWESOME");speak(answer + " Is AWESOME");
+	
+	}
+// -- tell the user whatever they entered is awesome!
 
-	// 6. If the random number is 1
+	
+// 6. If the random number is 1
 
-	// -- tell the user whatever they entered is ok.
+if (random == 1) {
+	JOptionPane.showMessageDialog(null, answer + " Is good.......");speak( answer + " Is good");}
 
-	// 7. If the random number is 2
+// -- tell the user whatever they entered is OK.
 
-	// -- tell the user whatever they entered is boring.
 
-	// 8. If the random number is 3
+// 7. If the random number is 2
+if (random == 2) {
+	JOptionPane.showMessageDialog(null, answer + " Is uh ok......");speak( answer +" Is uh ok");}
 
-	// -- write your own answer
+// -- tell the user whatever they entered is boring.
 
+	
+// 8. If the random number is 3
+if (random == 3) {
+	JOptionPane.showMessageDialog(null, " ugh it's " + answer + "");speak(" ugh it's " + answer + "");}
+
+// -- write your own answer
+}
+static void speak(String words) {
+	try {
+		Runtime.getRuntime().exec("say " + words).waitFor();
+	}
+	 catch (Exception e) {
+		 e.printStackTrace();
+	 }
+}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,34 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
-		
+		int score = 0;
 		// 1.  Create a variable to hold the user's score 
-		
+		for (int i = 0 ; i< 10;i++) {
 		// 2.  Ask the user a question 
-		
+		String answer = JOptionPane.showInputDialog("Whats your name?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+	 { 
+			score++; 
+			JOptionPane.showMessageDialog(null, "(^_^)");
 		// 4.  if the user's answer is correct
-		
+			String answer2 = JOptionPane.showInputDialog("Whats the color of the sky?");
 		// -- add one to their score 
-		
+					if(answer2.equals("blue"));
+					JOptionPane.showMessageDialog(null, "(^_^)");
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+					String answer3 = JOptionPane.showInputDialog("Whats the opposite of down?");
+					
+					JOptionPane.showMessageDialog(null, ""+ answer + " " + answer2 + " " + answer3 + ".");
+					
+					JOptionPane.showMessageDialog(null," lol");
+					
+
+		// 6.  After all the questions have been asked, print the user's score.
+		}
+	}
 	}
 }
